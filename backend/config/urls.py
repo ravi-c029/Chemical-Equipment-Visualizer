11be55251d3276741562158fdc45e699738ea9ff
ravi-c029 <ravi.keshari029@gmail.com>
2026-02-03 19:41:53 +0530

    Move secret admin link to main urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core.views import create_admin_user
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')), # Include our core app URLs here
+    path('fix-admin/', create_admin_user), 
 ]
+
 
 # This is required to serve the uploaded CSV files during development
 if settings.DEBUG:
