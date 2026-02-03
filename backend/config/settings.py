34ffd02fe684805d23f897a0fbc1291d36af96ea
ravi-c029 <ravi.keshari029@gmail.com>
2026-02-03 19:26:12 +0530

    Add corsheaders to requirements and installed apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['chemical-equipment-visualizer-wptm.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third-party apps
     'rest_framework',
-    'corsheaders',
+    
 
     # Local apps
     'core',
